b03ebf6c5bf6713e3f2a1e2ac6cb7c823a39dee2
TaeHyun Kim <th031017@naver.com>
2025-06-07 13:36:18 +0900

    [기능추가] 목표 최대 지출 설정 및 총 지출액에 따른 문구 출력 기능 추가

--- OSS_Budget/budget.py
+++ OSS_Budget/budget.py
@@ -4,6 +4,7 @@ from expense import Expense
 class Budget:
     def __init__(self):
         self.expenses = []
+        self.goal_expense = 0
 
     def add_expense(self, category, description, amount):
         today = datetime.date.today().isoformat()
@@ -19,6 +20,37 @@ class Budget:
         for idx, e in enumerate(self.expenses, 1):
             print(f"{idx}. {e}")
         print()
+    # 목표 최대 지출액 설정 함수
+    def set_goal_expense(self, goal):
+        self.goal_expense = goal
+        print("목표 최대 지출액이 설정되었습니다.\n")
+    # 목표 최대 지출액 달성도에 따른 문구 출력
+    def percent_phrase(self):
+        # 목표 최대 지출 설정 안했을 시 return
+        if self.goal_expense <= 0:
+            print("목표 최대 지출액을 설정해보는게 어떨까요?\n")
+            return
+        
+        print(f"목표 최대 지출액: {self.goal_expense}원\n")
+        
+        total = sum(e.amount for e in self.expenses)
+        percent = total / self.goal_expense * 100
+        if percent <= 10:
+            print(f"목표 최대 지출액의 {percent:.1f}% 사용! 최대한 아껴봐요!\n")
+        elif 10 < percent <= 25:
+            print(f"목표 최대 지출액의 {percent:.1f}% 사용! 아직은 여유로워요!\n")
+        elif 25 < percent <= 50:
+            print(f"목표 최대 지출액의 {percent:.1f}% 사용! 음 아직은 괜찮나..?\n")
+        elif 50 < percent <= 75:
+            print(f"목표 최대 지출액의 {percent:.1f}% 사용! 어라.. 벌써 절반 넘게 썼어요\n")
+        elif 75 < percent <= 90:
+            print(f"목표 최대 지출액의 {percent:.1f}% 사용! 목표 달성하려면 이제 허리띠 꽉 졸라매야겠는데요?\n")
+        elif 90 < percent < 100:
+            print(f"목표 최대 지출액의 {percent:.1f}% 사용! 이젠 정말 아껴야 해요!\n")
+        elif percent == 100:
+            print("목표 최대 지출액에 도달했어요! 되도록이면 더 쓰지 않는게 어떨까요?\n")
+        else:
+            print("목표 최대 지출액을 초과했어요... 다음엔 지켜봐요!\n")
 
     def total_spent(self):
         total = sum(e.amount for e in self.expenses)
--- OSS_Budget/main.py
+++ OSS_Budget/main.py
@@ -9,7 +9,8 @@ def main():
         print("1. 지출 추가")
         print("2. 지출 목록 보기")
         print("3. 총 지출 보기")
-        print("4. 종료")
+        print("4. 목표 최대 지출액 입력")
+        print("5. 종료")
         choice = input("선택 > ")
 
         if choice == "1":
@@ -24,11 +25,28 @@ def main():
 
         elif choice == "2":
             budget.list_expenses()
-
+        
+        # 총 지출 보기 선택 시 
+        # 목표 최대 지출 설정X -> 총지출만 출력
+        # 목표 최대 지출 설정O -> 총지출, 목표 최대 지출 및 문구 출력력
         elif choice == "3":
             budget.total_spent()
-
+            budget.percent_phrase()
+        
+        # 목표 최대 지출액 설정
         elif choice == "4":
+            try:
+                goal = int(input("목표 최대 지출액(원): "))
+                if goal <= 0:
+                    print("0보다 큰 값을 입력하세요\n")
+                    continue
+                budget.set_goal_expense(goal)
+            except ValueError:
+                print("잘못된 금액입니다.\n")
+                continue
+
+
+        elif choice == "5":
             print("가계부를 종료합니다.")
             break
 
